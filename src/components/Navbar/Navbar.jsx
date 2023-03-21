9cc6412345f3a97b48dcb8a8f3a39e1dcf167b5c
BartoszJS <bartosz290300@wp.pl>
2023-03-21 22:09:29 +0100

    1% first commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import { Link } from "react-router-dom";
+import "./Navbar.scss";
+
+const Navbar = () => {
+  return (
+    <header className='navbar'>
+      <div className='wrapper'>
+        <div className='left'>
+          <div className='item'>
+            EN
+            <KeyboardArrowDownIcon />
+          </div>
+          <div className='item'>
+            USD
+            <KeyboardArrowDownIcon />
+          </div>
+          <div className='item'>
+            <Link className='link' to='/products/1'>
+              Women
+            </Link>
+          </div>
+          <div className='item'>
+            <Link className='link' to='/products/2'>
+              Men
+            </Link>
+          </div>
+          <div className='item'>
+            <Link className='link' to='/products/3'>
+              Children
+            </Link>
+          </div>
+        </div>
+        <div className='center'>
+          <Link className='link' to='/'>
+            HOME
+          </Link>
+        </div>
+        <div className='right'>
+          <div className='item'>
+            <Link className='link' to='/'>
+              Homepage
+            </Link>
+          </div>
+          <div className='item'>
+            <Link className='link' to='/'>
+              About
+            </Link>
+          </div>
+          <div className='item'>
+            <Link className='link' to='/'>
+              Contact
+            </Link>
+          </div>
+          <div className='item'>
+            <Link className='link' to='/'>
+              Stores
+            </Link>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
